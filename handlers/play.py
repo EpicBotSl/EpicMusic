e462ccd01db64c1bea9299571f5ed1ec2279d4de
NAVANJANA <104069319+Captainamarica@users.noreply.github.com>
2022-06-03 15:43:18 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -77,7 +77,7 @@ async def play(_, message: Message):
                     invitelink = await _.export_chat_invite_link(chid)
                 except:
                     await fallen.edit(
-                        "<b>» ꜰɪʀsᴛʟʏ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ʙᴀʙʏ</b>")
+                        "<b>» ꜰɪʀsᴛʟʏ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ </b>")
                     return
 
                 try:
